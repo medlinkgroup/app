3c38740ea05b2f0f657104c03c9c3b369ed2a622
Fabiana <fabimontiel@gmail.com>
2020-03-30 20:12:59 +0200

    initial screens, design not decided

---
+++ medlink/medlink/DocProfileListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DocProfileListViewController.swift
+//  medlink
+//
+//  Created by Fabiana Montiel on 23/03/2020.
+//  Copyright © 2020 Fabiana Montiel. All rights reserved.
+//
+
+import UIKit
+
+class DocProfileListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ medlink/medlink/HomeSignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeSignUpViewController.swift
+//  medlink
+//
+//  Created by Fabiana Montiel on 23/03/2020.
+//  Copyright © 2020 Fabiana Montiel. All rights reserved.
+//
+
+import UIKit
+
+class HomeSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
