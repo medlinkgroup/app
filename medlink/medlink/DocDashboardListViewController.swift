2977e002ff545dfe1288717317877e33fdab3189
cylia06 <cylia.silouh@gmail.com>
2020-06-16 15:03:21 +0200

    login ok

--- medlink/medlink/DocDashboardListViewController.swift
+++ medlink/medlink/DocDashboardListViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  DocDashboardListViewController.swift
 //  medlink
--- medlink/medlink/HomeViewController.swift
+++ medlink/medlink/HomeViewController.swift
@@ -14,25 +14,13 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var login_Btn: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
-    
-    @IBOutlet var label_login_title: UILabel!
-    @IBOutlet var label_username: UILabel!
-    @IBOutlet var label_password: UILabel!
-    @IBOutlet var btn_login: UIButton!
-    @IBOutlet var signUp_button: UIButton!
-    
-    
+    @IBOutlet weak var signUpBtn: UIButton!
+       
     override func viewDidLoad() {
         super.viewDidLoad()
+
         AuthCheck()
-        
-        label_login_title.text = NSLocalizedString("login_title", comment: "")
-        label_username.text = NSLocalizedString("username", comment: "")
-        label_password.text = NSLocalizedString("password", comment: "")
-        signUp_button.setTitle(NSLocalizedString("signup", comment: ""), for: .normal)
-        signUp_button.titleLabel?.textAlignment = .center
-        
-        //errorLabel.alpha = 0
+        errorLabel.alpha = 0
 
         // Do any additional setup after loading the view.
     }
@@ -54,39 +42,18 @@ class HomeViewController: UIViewController {
     private func AuthCheck(){
 
            if (Auth.auth().currentUser?.uid) != nil {
-                 self.navigationController?.setViewControllers([DocAccountViewController()], animated: false)
+                 self.navigationController?.setViewControllers([DocHomeViewController()], animated: false)
            }
            
     }
        
-    /*@IBAction func signUpBtn(_ sender: Any) {
-        self.navigationController?.pushViewController(HomeSignUpViewController(), animated: true)
-    }*/
-    
-    @IBAction func go_to_sign_up(_ sender: Any) {
+  
+    @IBAction func signUpBtn(_ sender: Any) {
         self.navigationController?.pushViewController(HomeSignUpViewController(), animated: true)
     }
+   
     
-    @IBAction func login_btn(_ sender: Any) {
-        let error = valedateFields()
-        if error != nil {
-               showError(error!)
-        }else {
-            // create cleaned textfield
-            let email = loginTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-            // connection
-            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
-                if error != nil {
-                    self.showError("Mot de passe incorrect")
-                } else {
-                    self.navigationController?.setViewControllers([DocProfileListViewController()], animated: true)
-                }
-            })
-        }
-    }
-    
-    /*@IBAction func loginBtn(_ sender: Any) {
+    @IBAction func loginBtn(_ sender: Any) {
         let error = valedateFields()
                if error != nil {
                       showError(error!)
@@ -99,12 +66,12 @@ class HomeViewController: UIViewController {
                        if error != nil {
                            self.showError("Mot de passe incorrect")
                        } else {
-                           self.navigationController?.setViewControllers([DocProfileListViewController()], animated: true)
+                           self.navigationController?.setViewControllers([DocHomeViewController()], animated: true)
                        }
                    })
                }
      
-    }*/
+    }
     
     
     
