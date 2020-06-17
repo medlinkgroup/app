78b1668590539f341a0c6fb1b874c915e06399d6
cylia06 <cylia.silouh@gmail.com>
2020-06-17 18:57:55 +0200

    list patients ok

--- medlink/medlink/PatientsListViewController.swift
+++ medlink/medlink/PatientsListViewController.swift
@@ -11,7 +11,7 @@ import Firebase
 import FirebaseFirestore
 import FirebaseAuth
 
-class PatientsListViewController: UIViewController {
+class PatientsListViewController: UIViewController, UITableViewDataSource , UITableViewDelegate{
 
     @IBOutlet weak var label_patients: UILabel!
     @IBOutlet weak var label_my_patients: UILabel!
@@ -19,54 +19,73 @@ class PatientsListViewController: UIViewController {
     
     public static let patientsTableViewCellId = "ptvc"
     
-    var patients: [Patient] = [] {
-        didSet {
-            self.tableview_list_patients.reloadData()
-        }
-    }
     var patientService: PatientService{
-        return PatientMockService()
-    }
+           return PatientMockService()
+       }
+   
+   
+    var DoctorUID: String?
     
+    var patients: [Patient] = [] {
+           didSet {
+               self.tableview_list_patients.reloadData()
+           }
+       }
+    override func viewDidLoad() {
+           super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = false
+        
+        self.tableview_list_patients.rowHeight = 120
+        self.tableview_list_patients.register(UINib(nibName: "PatientsTableViewCell", bundle: nil), forCellReuseIdentifier: PatientsListViewController.patientsTableViewCellId)
+        self.tableview_list_patients.dataSource = self
+        self.tableview_list_patients.delegate = self
+        let db = Firestore.firestore()
+        if let user = Auth.auth().currentUser{
+           let docRef = db.collection("users").document(user.uid)
+                
+               docRef.getDocument { (document, error) in
+               if let document = document, document.exists {
+                   //print(document.data()!)
+                self.DoctorUID = document["uid"] as? String
+                   } else {
+                       print("Document does not exist")
+                   }
+               }
+            } else {
+                //fatalError(" Erreur : aucun user connect")
+                print("no user connected")
+            }
+        
+           
+           // Do any additional setup after loading the view.
+       }
     override func viewWillAppear(_ animated: Bool) {
-        self.patientService.getAll{(patients)in
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        self.patientService.getAll { (patients) in
+            print(patients)
             self.patients = patients
-            //self.sortData()
-            self.tableview_list_patients.reloadData()
+            // filter just doctorId
+            
+            print(self.patients)
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        AuthCheck()
-        
-        // Do any additional setup after loading the view.
-    }
     
-    private func AuthCheck(){
-
-       if (Auth.auth().currentUser?.uid) == nil {
-             self.navigationController?.setViewControllers([HomeViewController()], animated: false)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: PatientsListViewController.patientsTableViewCellId, for: indexPath) as! PatientsTableViewCell
+           let patient = self.patients[indexPath.row]
+           cell.label_patient_name.text = patient.lastName
+           return cell
        }
-           
-    }
-
-    /*
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    */
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         patients.count
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: PatientsListViewController.patientsTableViewCellId, for: indexPath) as! PatientsTableViewCell
-        let patient = self.patients[indexPath.row]
-        cell.label_patient_name.text = patient.lastName
-        return cell
-    }
+    private func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+           print("You tapped cell number \(indexPath.row).")
+       }
+   
     
     // when cell is selected
     /*
