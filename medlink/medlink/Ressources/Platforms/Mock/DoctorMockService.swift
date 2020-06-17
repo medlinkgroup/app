e6b6689e5c40a7bf68d2e02c41790d6347cf7c0c
Fabiana <fabimontiel@gmail.com>
2020-06-17 18:55:19 +0200

    more mock for doctors

--- medlink/medlink/Ressources/Platforms/Mock/DoctorMockService.swift
+++ medlink/medlink/Ressources/Platforms/Mock/DoctorMockService.swift
@@ -36,6 +36,24 @@ class DoctorMockService: DoctorService {
         email: "cylia@gmail.com",
         birthDate: "2019-12-26T00:00:00.000Z",
         field: "Action"
+        ),
+        Doctor(_id: "3",
+              firstName: "event3",
+              lastName: "event3des",
+              phone: "event3 freetaxt",
+              photo: URL(string:  "http://restaurant-legrey.fr/wp-content/uploads/elementor/thumbs/salle-2-grey-resto-5-nrnp7vsw82tzrpkdb6zrpyu7f9pplh7f9u1qbr6tpc.jpg"),
+              email: "f@gmail.com",
+              birthDate: "2020-11-12T00:00:00.000Z",
+              field: "Action"
+              ),
+        Doctor(_id: "4",
+              firstName: "event4",
+        lastName: "event4des",
+        phone: "event4 freetaxt",
+        photo: URL(string:  "http://restaurant-legrey.fr/wp-content/uploads/elementor/thumbs/salle-2-grey-resto-5-nrnp7vsw82tzrpkdb6zrpyu7f9pplh7f9u1qbr6tpc.jpg"),
+        email: "f@gmail.com",
+        birthDate: "2019-03-04T00:00:00.000Z",
+        field: "Action"
         )
         
     ]
