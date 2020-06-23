9095a7fe0415b4b76f0d432bcaf6ff839d5864e3
cylia06 <cylia.silouh@gmail.com>
2020-06-23 23:47:47 +0200

    fichier addPatient

--- medlink/medlink/AddPatientViewController.swift
+++ medlink/medlink/AddPatientViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FirebaseFirestore
 import FirebaseAuth
+import FirebaseStorage
 
 class AddPatientViewController: UIViewController,UITextFieldDelegate {
 
@@ -26,6 +27,7 @@ class AddPatientViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var PlaceText: UITextField!
     @IBOutlet weak var ImageURLText: UITextField!
     @IBOutlet weak var DateText: UITextField!
+    @IBOutlet weak var photoView: UIImageView!
     
     
     
@@ -60,9 +62,102 @@ class AddPatientViewController: UIViewController,UITextFieldDelegate {
                      } else {
                          fatalError(" Erreur : aucun user connect")
                      }
+                   self.FirstNameText.delegate = self
+                   self.LastNameText.delegate = self
+                   self.EmailText.delegate = self
+                   self.PhoneText.delegate = self
+                   self.PlaceText.delegate = self
+                   self.DateText.delegate = self
+                   self.ImageURLText.delegate = self
+                  
+                 
+                 //  createDatePicker()
+              
+                  
+                 //  imagePicker.delegate = self
                   
 
        
     }
+    
+    func uploadPatientImage(imageData: Data)  {
+        let activityIndicator = UIActivityIndicatorView.init(style: .gray)
+           activityIndicator.startAnimating()
+           activityIndicator.center = self.view.center
+           self.view.addSubview(activityIndicator)
+        
+           
+           let storageReference = Storage.storage().reference()
+           //let currentUser = Auth.auth().currentUser
+           
+           //let eventRef = db.collection("events").document()
+           var patientRef: DocumentReference? = nil
+           patientRef = db.collection("patients").addDocument(data: [
+                "PatientId": "1",
+                "patientImage": ""
+                ])
+           { err in
+                    if let err = err {
+                        print("Error adding document: \(err)")
+                    } else {
+                        print("Document added with ID: \(patientRef!.documentID)")
+                    }
+            }
 
+        let patientImageRef = storageReference.child("patients").child("\(patientRef!.documentID)-patientImage.jpg")
+
+        
+           let uploadMetaData = StorageMetadata()
+           uploadMetaData.contentType = "image/jpeg"
+           
+           patientImageRef.putData(imageData, metadata: uploadMetaData) { (uploadedImageMeta, error) in
+              
+               activityIndicator.stopAnimating()
+               activityIndicator.removeFromSuperview()
+               
+               if error != nil
+               {
+                   print("Error took place \(String(describing: error?.localizedDescription))")
+                   return
+               } else {
+                   
+                  // self.imageView.image = UIImage(data: imageData)
+                   
+                   print("Meta data of uploaded image \(String(describing: uploadedImageMeta))")
+                patientImageRef.downloadURL(completion: {(url, error) in
+                    if let urlText = url?.absoluteString
+                    {
+                        self.ImageURLText.text = urlText
+                        }
+                    })
+                }
+                }
+
+    }
+    
+
+}
+extension AddPatientViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      
+      if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage, let optimizedImageData =
+          editedImage.jpegData(compressionQuality: 0.5)
+      {
+          self.photoView.image = editedImage
+          //uploadPatientImage(imageData: optimizedImageData)
+      }
+      
+      if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage ,let optimizedImageData =
+         originalImage.jpegData(compressionQuality: 0.5)
+          {
+              self.photoView.image = originalImage
+             // uploadPatientImage(imageData: optimizedImageData)
+          }
+      dismiss(animated: true, completion: nil)
+  }
+
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        picker.dismiss(animated: true, completion:nil)
+    }
 }
