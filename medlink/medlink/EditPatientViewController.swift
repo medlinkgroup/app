504b02e7b6c157d5ad8f0b72547de9193e07561d
cylia06 <cylia.silouh@gmail.com>
2020-06-28 00:55:42 +0200

    edit patient ok

--- medlink/medlink/EditPatientViewController.swift
+++ medlink/medlink/EditPatientViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 import CoreLocation
+import FirebaseFirestore
+import FirebaseStorage
+import MobileCoreServices
 
-class EditPatientViewController: UIViewController {
+class EditPatientViewController: UIViewController, UIPickerViewDelegate {
 
     
     @IBOutlet var label_modify_patient: UILabel!
@@ -33,15 +36,25 @@ class EditPatientViewController: UIViewController {
     @IBOutlet var DateText: UITextField!
     @IBOutlet var PhotoView: UIImageView!
     @IBOutlet var ObjectText: UITextField!
+    let datePicker = UIDatePicker()
+    let objectPicker = UIPickerView()
+    let imagePicker = UIImagePickerController()
     
-    
-    
+    var objects = [Objet] ()
+    var selectedObjectId: String?
+    var selectedObjectName: String?
     var objet : Objet!
     var editPatient : Patient!
-    
+    var id : String?
+    var doctorUID : String?
+       let db = Firestore.firestore()
     var objetService: ObjetService{
         return ObjetApiService()
     }
+    var patientService: PatientService{
+          return PatientAPIService()
+          //return PatientMockService()
+        }
     override func viewDidAppear(_ animated: Bool) {
        
                self.objetService.getAll { (objets) in
@@ -49,11 +62,20 @@ class EditPatientViewController: UIViewController {
                    self.objet = objets.first(where: { (objet) -> Bool in
                     objet._id == self.editPatient.objetUid
                        })
+                
                 if (self.objet != nil){
                     self.ObjectText.text = self.objet.name
-                }
-                      
             }
+        self.objetService.getAll { (objects) in
+                      print(objects)
+                      self.objects = objects.filter({$0.isAttributed == false})
+                      // filter just doctorId
+                      
+                      print(self.objects)
+                  }
+        
+        }
+        
     }
     
     override func viewDidLoad() {
@@ -78,7 +100,11 @@ class EditPatientViewController: UIViewController {
         label_adress.text = NSLocalizedString("adress", comment: "")
         label_email.text = NSLocalizedString("email", comment: "")
         // Do any additional setup after loading the view.
+        self.id = editPatient._id
+        self.doctorUID = editPatient.doctorUid
         loadData()
+        createDatePicker()
+        createObjetPicker()
     }
 
 
@@ -88,7 +114,79 @@ class EditPatientViewController: UIViewController {
             return view
             
         }
-     
+    
+    @IBAction func upload_image_btn(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+                                       imagePicker.mediaTypes = [kUTTypeImage as String]
+                                       imagePicker.delegate = self
+                                       present(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func save_btn(_ sender: Any) {
+        guard let place = PlaceText.text,
+              let firstName = FirstNameText.text,
+              let lastName = LastNameText.text,
+              let email = EmailText.text,
+              let date = DateText.text,
+              let phone = PhoneText.text,
+
+              let imageURL = ImageURLText.text,
+            let  patient =  ObjectText.text,
+            
+              firstName.count > 0,
+              lastName.count > 0,
+              email.count > 0,
+              place.count > 0,
+              date.count > 0,
+              phone.count > 0
+              
+            //objetUid.count > 0
+             else {
+                self.displayError(message: "Missing required field")
+            return
+        }
+          
+          let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(place) { (placemarks, err) in
+            guard err == nil,
+                  let allPlacemarks = placemarks,
+                  let placeloc = allPlacemarks.first,
+                  let loc = placeloc.location else {
+                    self.displayError(message: "Address not found")
+                    return
+            }
+        
+            
+        
+            self.patientService.edit(id: self.editPatient._id, firstName: firstName, lastName: lastName, phone: phone, photo: imageURL, email: email, doctorUid: self.doctorUID!, place: place, location: loc, birthDate: date, objetUid: self.selectedObjectId ?? "")
+         
+          { (success) in
+              print (loc)
+              print(success)
+           
+             }
+                                 
+        }
+        if(selectedObjectId != nil && selectedObjectName != nil ){
+                              self.objetService.edit( id: selectedObjectId! , name: selectedObjectName! , isAttributed: true){ (success) in
+                                  print(success)
+                              }
+               }
+        if( objet != nil ){
+                self.objetService.edit( id: objet._id , name: objet.name , isAttributed: false){ (success) in
+                print(success)
+                   }
+            }
+       
+          let confirmationAlert = UIAlertController(title: "Succes", message: " creation succes.", preferredStyle: UIAlertController.Style.alert)
+                             confirmationAlert.addAction(UIAlertAction(title: "OK", style: .cancel, handler:nil ))
+                              
+                           
+                             self.present(confirmationAlert, animated: true, completion: nil)
+            
+          
+        
+    }
+    
     func loadData() {
            if (editPatient != nil){
                
@@ -122,6 +220,151 @@ class EditPatientViewController: UIViewController {
         self.PlaceText.text = editPatient.place
        }
       
-     
+     func displayError(message: String) {
+                  let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+                  alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                  self.present(alert, animated: true)
+              }
+        func createObjetPicker(){
+            //categoryPicker.tag = 1
+                    ObjectText.inputView = objectPicker
+                    objectPicker.delegate = self
+                    let toolBar = UIToolbar()
+                    toolBar.sizeToFit()
+                    let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self,
+                    action: #selector(AddPatientViewController.doneObjetPicker))
+                    toolBar.setItems([doneButton], animated: false)
+                    toolBar.isUserInteractionEnabled = true
+                    ObjectText.inputAccessoryView = toolBar
+        }
+        @objc func doneObjetPicker() {
+            
+            self.view.endEditing(true)
+                    
+        }
+    
+    func createDatePicker(){
+       // datePicker.datePickerMode = .date
+        let toolbar = UIToolbar()
+         toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(AddPatientViewController.donedatePicker ))
+        toolbar.setItems([doneButton], animated: true)
+        toolbar.isUserInteractionEnabled = true
+    DateText.inputAccessoryView = toolbar
+    DateText.inputView = datePicker
+        
+    }
+    @objc func donedatePicker(){
+
+      let formatter = DateFormatter()
+      formatter.dateFormat = "yyyy-MM-dd"
+        DateText.text = formatter.string(from: datePicker.date)
+      self.view.endEditing(true)
+    }
+    func uploadPatientImage(imageData: Data)  {
+           let activityIndicator = UIActivityIndicatorView.init(style: .gray)
+              activityIndicator.startAnimating()
+              activityIndicator.center = self.view.center
+              self.view.addSubview(activityIndicator)
+           
+              
+              let storageReference = Storage.storage().reference()
+              //let currentUser = Auth.auth().currentUser
+              
+              //let eventRef = db.collection("events").document()
+              var patientRef: DocumentReference? = nil
+              patientRef = db.collection("patients").addDocument(data: [
+                   "PatientId": "1",
+                   "patientImage": ""
+                   ])
+              { err in
+                       if let err = err {
+                           print("Error adding document: \(err)")
+                       } else {
+                           print("Document added with ID: \(patientRef!.documentID)")
+                       }
+               }
+
+           let patientImageRef = storageReference.child("patients").child("\(patientRef!.documentID)-patientImage.jpg")
+
+           
+              let uploadMetaData = StorageMetadata()
+              uploadMetaData.contentType = "image/jpeg"
+              
+              patientImageRef.putData(imageData, metadata: uploadMetaData) { (uploadedImageMeta, error) in
+                 
+                  activityIndicator.stopAnimating()
+                  activityIndicator.removeFromSuperview()
+                  
+                  if error != nil
+                  {
+                      print("Error took place \(String(describing: error?.localizedDescription))")
+                      return
+                  } else {
+                      
+                     // self.imageView.image = UIImage(data: imageData)
+                      
+                      print("Meta data of uploaded image \(String(describing: uploadedImageMeta))")
+                   patientImageRef.downloadURL(completion: {(url, error) in
+                       if let urlText = url?.absoluteString
+                       {
+                           self.ImageURLText.text = urlText
+                           }
+                       })
+                   }
+                   }
+
+       }
+       
 
 }
+extension EditPatientViewController:  UIImagePickerControllerDelegate,
+UIPickerViewDataSource, UINavigationControllerDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return objects.count
+        print(objects.count)
+    }
+    
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+           //return patientsName[row]
+           return objects[row].name
+       }
+       
+       
+       func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+           
+           //selectedPatient = patientsName[row]
+           selectedObjectId = objects[row]._id
+           selectedObjectName = objects[row].name
+        print(selectedObjectName)
+           ObjectText.text = selectedObjectName
+        print(ObjectText)
+       }
+func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      
+      if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage, let optimizedImageData =
+          editedImage.jpegData(compressionQuality: 0.5)
+      {
+          self.PhotoView.image = editedImage
+          uploadPatientImage(imageData: optimizedImageData)
+      }
+      
+      if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage ,let optimizedImageData =
+         originalImage.jpegData(compressionQuality: 0.5)
+          {
+              self.PhotoView.image = originalImage
+              uploadPatientImage(imageData: optimizedImageData)
+          }
+      dismiss(animated: true, completion: nil)
+  }
+
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        picker.dismiss(animated: true, completion:nil)
+    }
+}
+
